7e910cfca1b0cb067d05bf4951a253d882428298
IgorChukarin <Zigor2356@mail.ru>
2024-09-07 17:08:12 +0300

    solve creating temporary pdf in output directory

--- src/main/java/org/example/converters/XlsxConverter.java
+++ src/main/java/org/example/converters/XlsxConverter.java
@@ -7,36 +7,45 @@ import com.aspose.cells.WorksheetCollection;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 
 public class XlsxConverter {
 
     public static BufferedImage xlsxToImage(File xlsxFile) {
+        File tempPdfFile = null;
         try {
+
+            tempPdfFile = File.createTempFile("temp", ".pdf");
+            tempPdfFile.deleteOnExit();
+            System.out.println("Temporary file created at: " + tempPdfFile.getAbsolutePath());
+
             String xlsxPath = xlsxFile.getAbsolutePath();
-            String pdfPath = "C:/Users/Zigor/Desktop/TestFiles/output.pdf";
 
-            // Открытие исходного Workbook
             Workbook originalWorkbook = new Workbook(xlsxPath);
-
-            // Создание нового Workbook для сохранения только первого листа
             Workbook newWorkbook = new Workbook();
             WorksheetCollection originalSheets = originalWorkbook.getWorksheets();
             Worksheet firstSheet = originalSheets.get(0);
 
-            // Клонирование первого листа в новый Workbook
             Worksheet newSheet = newWorkbook.getWorksheets().get(0);
             newSheet.copy(firstSheet);
 
-            // Сохранение нового Workbook в PDF
             PdfSaveOptions pdfSaveOptions = new PdfSaveOptions();
-            newWorkbook.save(pdfPath, pdfSaveOptions);
+            newWorkbook.save(tempPdfFile.getAbsolutePath(), pdfSaveOptions);
 
-            File pdfFile = new File(pdfPath);
-            return PdfConverter.pdfToImage(pdfFile);
+            return PdfConverter.pdfToImage(tempPdfFile);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        } finally {
+            if (tempPdfFile != null && tempPdfFile.exists()) {
+                try {
+                    Files.delete(tempPdfFile.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
